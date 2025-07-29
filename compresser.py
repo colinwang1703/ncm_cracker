5a094e5bea6dd52ee69bc6c285b2b8c4c46f49fd
Colin Wang <colins.wang@hotmail.com>
2025-07-29 16:56:31 +0800

    LICENSE

--- compresser.py
+++ compresser.py
@@ -1,3 +1,17 @@
+# This file is part of ncm_cracker.
+#
+# ncm_cracker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ncm_cracker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ncm_cracker.  If not, see <https://www.gnu.org/licenses/>.
 """
 🚀 超快速音频压缩器
 使用多种优化技术：
--- project_manager.py
+++ project_manager.py
@@ -1,3 +1,17 @@
+# This file is part of ncm_cracker.
+#
+# ncm_cracker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ncm_cracker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ncm_cracker.  If not, see <https://www.gnu.org/licenses/>.
 """
 📁 项目目录结构管理器
 自动创建和管理文件夹结构
